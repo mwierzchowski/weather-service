af39785610784235546e10a1f4afbffd5a6fefd2
Marcin Wierzchowski <mwierzch@gmail.com>
2020-10-24 22:44:57 +0200

    Fix CI/CD

--- src/test/java/com/github/mwierzchowski/weather/WeatherServiceApplicationTests.java
+++ src/test/java/com/github/mwierzchowski/weather/WeatherServiceApplicationTests.java
@@ -1,7 +1,6 @@
 package com.github.mwierzchowski.weather;
 
 import com.github.mwierzchowski.weather.controller.WeatherController;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
