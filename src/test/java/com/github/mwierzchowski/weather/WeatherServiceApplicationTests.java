0b96af0cdf7666f6120a7a924916acbd1469b8c1
Marcin Wierzchowski <mwierzch@gmail.com>
2020-10-24 22:36:21 +0200

    Fix CI/CD

---
+++ src/test/java/com/github/mwierzchowski/weather/WeatherServiceApplicationTests.java
@@ -0,0 +1,21 @@
+package com.github.mwierzchowski.weather;
+
+import com.github.mwierzchowski.weather.controller.WeatherController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class WeatherServiceApplicationTests {
+	@Autowired
+	WeatherController controller;
+
+	@Test
+	void contextLoads() {
+		assertNotNull(controller);
+	}
+
+}
